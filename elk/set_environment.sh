caf2a7cdc6896f6f176435003571b3d503991fdb
Alfred Arouna <alfred+git@arouna.net>
2019-02-17 01:53:32 +0100

    Add comment for environment

--- elk/set_environment.sh
+++ elk/set_environment.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Create server key and certificate
 openssl req -subj '/CN=docker.elk /' -x509 -days 3650 -nodes -batch -newkey rsa:2048 -keyout pki/tls/private/logstash.key -out pki/tls/certs/logstash.crt
+# Create required folders
+mkdir -p elk/pki/tls/certs && mkdir -p elk/pki/tls/private
 # Set vm.max_map_count
 sudo sysctl vm.max_map_count=262144
